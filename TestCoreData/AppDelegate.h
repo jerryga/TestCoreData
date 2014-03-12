ca1ad8330b1b855cec6c02f399be0512195cad69
Static Ga <staticga@gmail.com>
2014-03-12 16:30:49 +0800

    Initial Commit

---
+++ TestCoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TestCoreData
+//
+//  Created by Static Ga on 14-3-12.
+//  Copyright (c) 2014年 Static Ga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TestCoreData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestCoreData
+//
+//  Created by Static Ga on 14-3-12.
+//  Copyright (c) 2014年 Static Ga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
